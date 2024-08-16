8767381fa1c81c44896137a92c1079b6b30ba1de
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2024-08-16 12:42:27 +0200

    Pass functions as values

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -9,9 +11,10 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png',
-          width: 300,
-          color: const Color.fromARGB(150, 255, 255, 255),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
           const SizedBox(height: 80),
           const Text(
@@ -23,13 +26,13 @@ class StartScreen extends StatelessWidget {
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-            onPressed: (){},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
             icon: const Icon(Icons.arrow_right_alt),
             label: const Text('Start quiz'),
-            )
+          )
         ],
       ),
     );
